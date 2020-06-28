cc2d32e1143629bc3a18a1bdf3e901b26c981b89
byarbrough <bcynmelk+git@gmail.com>
2020-06-28 11:25:49 -0400

    Default to world if no name

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Brian")
-	want := "Hello, Brian"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectGreeting := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("say hello to people", func(t *testing.T) {
+		got := Hello("Brian")
+		want := "Hello, Brian"
+		assertCorrectGreeting(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' if no name", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectGreeting(t, got, want)
+	})
+
 }
